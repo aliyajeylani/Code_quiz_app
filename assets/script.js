54fd0526f831ced61e4559893c676e5a3e090311
aliyajeylani <aliya.jeylani24@gmail.com>
2023-01-04 19:34:56 -0600

    added functionality to advance to the next code

--- assets/script.js
+++ assets/script.js
@@ -8,55 +8,46 @@ var answerButton2 = document.querySelector(".answer_button_2");
 var answerButton3 = document.querySelector(".answer_button_3");
 var answerButton4 = document.querySelector(".answer_button_4");
 var message = document.querySelector(".message");
+var currentQuestion = 0;
 
 
 var timeLeft = 90;
-var firstQuestion = [
+var questions = [
     {
         question: "Commonly used data types DO Not Include:",
         answers: ["1. Strings", "2. Booleans", "3. Alerts", "4. Numbers"],
-        correctAnswer: "Alerts"
-    }
-]
-
-var secondQuestion = [
+        correctAnswer: "3. Alerts"
+    },
     {
         question: "The condition is an if/else statement is enclosed with _________.",
         answers: ["1. Quotes", "2. Curly brackets", "3. Parenthesis", "4. Square brackets"],
         correctAnswer: "Curly brackets"
-    }
-]
-
-var thirdQuestion = [
+    },
     {
         question: "Arrays in JavaScript can be used to store _________.",
         answers: ["1. Numbers and strings", "2. Other arrays", "3. Booleans", "4. All of the above"],
         correctAnswer: "All of the above"
-    }
-]
-
-var fourthQuestion = [
+    },
     {
         question: "String values must be enclosed within _______ when being assigned to variables.",
         answers: ["1. Commas", "2. Curly brackets", "3. Quotes", "4. Parenthesis"],
         correctAnswer: "Quotes"
-    }
-]
-var fifthQuestion = [
+    },
     {
+
         question: "A very useful tool used during development and debugging for printing content to the debugger is:",
         answers: ["1. JavaScript", "2. Terminal/Bash", "3. For Loops", "4. Console.log"],
         correctAnswer: "Console.log"
     }
+
 ]
 
-
 startButton.addEventListener("click", function () {
-    question.textContent = firstQuestion[0].question;
-    answerButton1.innerHTML = firstQuestion[0].answers[0]
-    answerButton2.innerHTML = firstQuestion[0].answers[1]
-    answerButton3.innerHTML = firstQuestion[0].answers[2]
-    answerButton4.innerHTML = firstQuestion[0].answers[3]
+    question.textContent = questions[currentQuestion].question;
+    answerButton1.innerHTML = questions[currentQuestion].answers[0]
+    answerButton2.innerHTML = questions[currentQuestion].answers[1]
+    answerButton3.innerHTML = questions[currentQuestion].answers[2]
+    answerButton4.innerHTML = questions[currentQuestion].answers[3]
     quiz.style = "display: none";
     startButton.style = "display: none";
     answerbuttons.style.display = "flex";
@@ -72,20 +63,28 @@ startButton.addEventListener("click", function () {
     }, 1000);
 });
 
-answerbuttons.addEventListener("click" function () {
+answerbuttons.addEventListener("click", function (event) {
 
-    if (answer === correctAnswer) {
-        message.style.display = "block";
-        message.textContent = "Right Answer";
-    } else {
-        message.style.display = "block";
-        message.textContent = "Wrong Answer";
+    var userChoice = event.target;
+    console.log("Selection: " + userChoice.innerHTML);
 
+    var question1Answer = userChoice.innerHTML;
+
+    if (question1Answer !== questions[0].correctAnswer) {
+        timeLeft -= 10;
     }
 
+    currentQuestion += 1;
 
-}
+    question.textContent = questions[currentQuestion].question;
+    answerButton1.innerHTML = questions[currentQuestion].answers[0]
+    answerButton2.innerHTML = questions[currentQuestion].answers[1]
+    answerButton3.innerHTML = questions[currentQuestion].answers[2]
+    answerButton4.innerHTML = questions[currentQuestion].answers[3]
+
+})
 
 
-);
+
+
 
